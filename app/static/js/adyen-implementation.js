6ed3b9dcb8bf5528afc7672a497c4e8f11275801
anamotaadyen <ana.mota@adyen.com>
2023-02-01 15:50:26 +0000

    load correct country from saved db

--- app/static/js/adyen-implementation.js
+++ app/static/js/adyen-implementation.js
@@ -341,6 +341,7 @@ async function onLoad() {
       loadConfig(getConfigResponse)
     }
   }
+  initCheckout()
 }
 
 // Function to add mutiple attributes to a div
@@ -349,6 +350,67 @@ function setAttributes(el, options) {
     el.setAttribute(attr, options[attr])
   })
 }
+
+// Load config based on the data from db
+function loadConfig(configData) {
+  filteredData = Object.fromEntries(Object.entries(configData).filter(([key]) => !key.match('showPayMethod') && !key.match('placeHolderData')))
+  for (const [key, value] of Object.entries(filteredData)) {
+    console.log(key, value)
+    if (value == true){
+      document.getElementById(key).checked = true
+    }
+  }
+  openFirst = configData.openFirst
+  billAdd = configData.billAdd
+  onlyStored = configData.onlyStored
+  holderName = configData.holderName 
+  showPayMethod = configData.showPayMethod
+  if (showPayMethod == true){
+    document.getElementById("showPayMethod").checked = false
+  }
+  else{
+    document.getElementById("showPayMethod").checked = true
+  }
+  hideCVC = configData.hideCVC
+  placeholderData = configData.placeholderData
+  if (placeholderData == false){
+    document.getElementById("placeholderData").checked = false
+  }
+  else {
+    document.getElementById("placeholderData").checked = true
+  }
+  instantArray = configData.instantArray
+  if (instantArray == []){
+    document.getElementById("instantPay").checked = false
+  }
+  else{
+    document.getElementById("instantPay").checked = true
+  }
+  payMethods = configData.payMethods
+  payArray = configData.payArray
+  countrySettings = configData.countrySettings
+  savedCountry = countrySettings.countryCode
+  let loadCountry = {
+      value: savedCountry
+  }
+  let countryDropdown = document.getElementById("country_select")
+  countryDropdown.value = savedCountry
+  document.getElementById("flag_img").src = flagUrlMap[savedCountry].src
+  changeSelect(loadCountry)
+}
+
+// Load saved style based on the data from db
+function loadStyle(styleData) {
+  for (const [key, value] of Object.entries(styleData)) {
+    r.style.setProperty(key, value);
+  }
+  if (styleData.merchantUrl){
+    let merchantURL = styleData.merchantUrl
+    loadMerchantLogo(merchantURL)
+  }
+    updateColorPickers()
+  }
+
 
 /**
  * @function createToggles - creates toggles to enable/disable payment methods according to /paymentMethods response
@@ -461,6 +523,7 @@ async function getToggles() {
 async function changeSelect(el) {
   console.log(el)
   console.log(Object.values)
+  console.log(document.getElementById("flag_img").src)
   document.getElementById("flag_img").src = flagUrlMap[el.value].src
   const country = el.value
   countrySettings = getCountryData(country)
@@ -678,8 +741,8 @@ async function getConfiguration() {
   let configuration = {
     paymentMethodsResponse: paymentMethodsResponse,
     clientKey,
-    locale: countrySettings.locale || "en_NL",
-    countryCode: countrySettings.countryCode || "NL",
+    locale: countrySettings.locale,
+    countryCode: countrySettings.countryCode,
     environment: "test",
     showPayButton: true,
     paymentMethodsConfiguration: {
@@ -1333,61 +1396,6 @@ function resetDynamicCSS() {
     updateColorPickers()
   }
 
-// Load saved style based on the data from db
-function loadStyle(styleData) {
-  for (const [key, value] of Object.entries(styleData)) {
-    r.style.setProperty(key, value);
-  }
-  if (styleData.merchantUrl){
-    let merchantURL = styleData.merchantUrl
-    loadMerchantLogo(merchantURL)
-  }
-    updateColorPickers()
-  }
-
-// Load config based on the data from db
-function loadConfig(configData) {
-  filteredData = Object.fromEntries(Object.entries(configData).filter(([key]) => !key.match('showPayMethod') && !key.match('placeHolderData')))
-  for (const [key, value] of Object.entries(filteredData)) {
-    console.log(key, value)
-    if (value == true){
-      document.getElementById(key).checked = true
-    }
-  }
-  openFirst = configData.openFirst
-  billAdd = configData.billAdd
-  onlyStored = configData.onlyStored
-  holderName = configData.holderName 
-  showPayMethod = configData.showPayMethod
-  if (showPayMethod == true){
-    document.getElementById("showPayMethod").checked = false
-  }
-  else{
-    document.getElementById("showPayMethod").checked = true
-  }
-  hideCVC = configData.hideCVC
-  placeholderData = configData.placeholderData
-  if (placeholderData == false){
-    document.getElementById("placeholderData").checked = false
-  }
-  else {
-    document.getElementById("placeholderData").checked = true
-  }
-  instantArray = configData.instantArray
-  if (instantArray == []){
-    document.getElementById("instantPay").checked = false
-  }
-  else{
-    document.getElementById("instantPay").checked = true
-  }
-  payMethods = configData.payMethods
-  payArray = configData.payArray
-  countrySettings = configData.countrySettings
-  savedCountry = countrySettings.countryCode
-  let countryDropdown = document.getElementById("country_select")
-  countryDropdown.value = savedCountry
-  document.getElementById("flag_img").src = flagUrlMap[savedCountry].src
-}
 
 // logging configuration object to UI
 function logConfig(cloneConfig) {
@@ -1604,4 +1612,4 @@ updateColorPickers()
 
 onLoad()
 
-initCheckout()
+// initCheckout()
