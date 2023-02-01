26510171a438c48743ac8f2073f127ae89cc42de
b-leech <bradleyleechh@gmail.com>
2023-02-01 13:14:51 +0000

    Fixed not removing logo bug
    Fixed bug where the lgoo does not reset.

--- app/static/js/adyen-implementation.js
+++ app/static/js/adyen-implementation.js
@@ -1292,9 +1292,10 @@ function resetDynamicCSS() {
     r.style.setProperty("--inputBorder-width", null)
     r.style.setProperty("--inputBorder-radius", null)
     r.style.setProperty("--logo-width", null)
-    r.style.setProperty(--logo-verPosition, null)
+    r.style.setProperty("--logo-verPosition", null)
     document.getElementById("banner").style.display = "none"
-    document.getElementById("merchantLogoUrl").value = null
+    document.getElementById("merchantLogoUrl").value = ''
+    document.querySelector('.logo').src = ''
     updateColorPickers()
   }
 
